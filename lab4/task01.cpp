554ff2d220ed1fe9d1526f61bf13159c69af0229
PontoZawrik <Psichotroniks@tut.by>
2025-09-24 11:42:24 +0300

    Задание 1

--- lab4/task01.cpp
+++ lab4/task01.cpp
@@ -1,5 +1,28 @@
 #include "main.h"
 
 void task01() {
-	cout << "фвыфвв";
+	double r, m;
+
+	cout << "Введите число r: ";
+	cin >> r;
+	cout << "Введите число m: ";
+	cin >> m;
+
+	double result;
+
+	if (r <= m && m < 2 * r) {
+		cout << "Ветка при r <= m < 2r: \n";
+		result = sqrt(fabs(3 * m - 5 * r)) * exp(m / r);
+	}
+	else if (m > 2 * r) {
+		cout << "Ветка при m > 2r: \n";
+		result = sqrt(fabs(3 * m + 5 * r)) * exp(r / m);
+	}
+	else {
+		cout << "Такой вариант не предусмотрен. \n";
+		return;
+	}
+
+	cout << "t2 = " << result << endl;
+	return;
 }
